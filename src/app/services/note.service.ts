c7c8ac77b977697a3fd2d3eff0fb3e23917a5828
TkachevNikita <ntkachev03@mail.ru>
2024-04-14 02:59:22 +0500

    chore: добавил метод добавления заметки

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -2,9 +2,12 @@ import {Injectable} from "@angular/core";
 import {NoteModel} from "../models/note.model";
 import {map, Observable, of} from "rxjs";
 import {INote} from "../interfaces/note.interface";
+import {StorageService} from "./storage.service";
 
 @Injectable()
 export class NoteService {
+
+    constructor(private readonly _storageService: StorageService) { }
 
     public getNotes(): Observable<NoteModel[]> {
         return this.requestNotes()
@@ -15,5 +18,10 @@ export class NoteService {
 
     private requestNotes(): Observable<INote[]> {
         return of(JSON.parse(localStorage.getItem('notes')!) || []);
+    }
+
+    public createNote(note: INote): void {
+        this._storageService.getStorage('key')
+            ? this._storageService.updateStorage('key', note) : this._storageService.createNewStorage('key', note);
     }
 }

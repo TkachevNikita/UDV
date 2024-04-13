d4d2fcdd740562568e7c1481d44644ff9de0fd38
TkachevNikita <ntkachev03@mail.ru>
2024-04-14 04:29:03 +0500

    chore: добавил компонент создания заметки на страницу

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {NoteComponent} from "./components/note/note.component";
 import {Observable} from "rxjs";
 import {AsyncPipe} from "@angular/common";
 import {NoteService} from "./services/note.service";
+import {CreateNoteComponent} from "./components/create-note/create-note.component";
 
 @Component({
     selector: 'app-root',
@@ -18,7 +19,8 @@ import {NoteService} from "./services/note.service";
         InputComponent,
         ReactiveFormsModule,
         NoteComponent,
-        AsyncPipe
+        AsyncPipe,
+        CreateNoteComponent
     ],
     providers: [
         NoteService
@@ -28,11 +30,33 @@ import {NoteService} from "./services/note.service";
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
+    public isCreating: boolean = false;
+
     public notes$!: Observable<NoteModel[]>;
 
     constructor(private readonly _noteService: NoteService) { }
 
     public ngOnInit(): void {
-        this.notes$ = this._noteService.getNotes();
+        this.notes$ = this._noteService.getNotes;
+    }
+
+    public createNote(title: string): void {
+        this._noteService.createNote({
+            id: crypto.randomUUID(),
+            title: title
+        });
+        this.isCreating = false;
+    }
+
+    public removeNote(id: string): void {
+        this._noteService.removeNote(id);
+    }
+
+    public onCreateClick(): void {
+        this.isCreating = true;
+    }
+
+    public onCloseCreateClick(): void {
+        this.isCreating = false;
     }
 }
